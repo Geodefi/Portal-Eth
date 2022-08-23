c56f5339a6eb6d83e837c9d6dfccce3ee2a948a5
geode-main <admin@geode.fi>
2022-08-23 23:38:16 +0300

    Refactor: DepositContractUtils.js

--- test/Libs/DepositContractUtils.js
+++ test/Libs/DepositContractUtils.js
@@ -5,7 +5,6 @@ const chai = require("chai");
 chai.use(solidity);
 const { expect } = chai;
 
-
 describe("DepositContractUtils", async () => {
   let testContract;
 
@@ -32,33 +31,57 @@ describe("DepositContractUtils", async () => {
     it("addressToWC to get 0x01 instead of 0x00 as beginning", async () => {
       const WCAddress = ethers.Wallet.createRandom().address;
       const response = await testContract.addressToWC(WCAddress);
-      const withdrawalCredential = "0x01" + "0000000000000000000000" + WCAddress.substring(2);
+      const withdrawalCredential =
+        "0x01" + "0000000000000000000000" + WCAddress.substring(2);
       expect(response).to.eq(withdrawalCredential.toLowerCase());
     });
 
-    describe("getDepositDataRoot from pubkey, withdrawal_credentials, signature and stakeAmount", async () => {
-      const pubkey = "0x91efd3ce6694bc034ad4c23773877da916ed878ff8376610633a9ae4b4d826f4086a6b9b5b197b5e148be658c66c4e9a";
-      const withdrawal_credentials = "0x004f58172d06b6d54c015d688511ad5656450933aff85dac123cd09410a0825c";
-      const signature = "0x8bbeff59e3016c98d7daf05ddbd0c71309fae34bf3e544d56ebff030b97bccb83c5abfaab437ec7c652bbafa19eb30661979b82e79fa351d65a50e3a854c1ef0c8537f97ceaf0f334096509cd52f716150e67e17c8085d9622f376553da51181";
-      const depositDataRoot = "0xcf73f30d1a20e2af0446c2630acc4392f888dc0532a09592e00faf90b2976ab8";
-      
+    describe("getDepositDataRoot from pubkey, withdrawalCredentials, signature and stakeAmount", async () => {
+      const pubkey =
+        "0x91efd3ce6694bc034ad4c23773877da916ed878ff8376610633a9ae4b4d826f4086a6b9b5b197b5e148be658c66c4e9a";
+      const withdrawalCredentials =
+        "0x004f58172d06b6d54c015d688511ad5656450933aff85dac123cd09410a0825c";
+      const signature =
+        "0x8bbeff59e3016c98d7daf05ddbd0c71309fae34bf3e544d56ebff030b97bccb83c5abfaab437ec7c652bbafa19eb30661979b82e79fa351d65a50e3a854c1ef0c8537f97ceaf0f334096509cd52f716150e67e17c8085d9622f376553da51181";
+      const depositDataRoot =
+        "0xcf73f30d1a20e2af0446c2630acc4392f888dc0532a09592e00faf90b2976ab8";
+
       it("reverts when stakeAmount less than 1 ether", async () => {
         const stakeAmount = 2e9;
-        await expect(testContract.getDepositDataRoot(pubkey, withdrawal_credentials, signature, stakeAmount)).to.be.revertedWith(
-          "DepositContract: deposit value too low"
-        );
+        await expect(
+          testContract.getDepositDataRoot(
+            pubkey,
+            withdrawalCredentials,
+            signature,
+            stakeAmount
+          )
+        ).to.be.revertedWith("DepositContract: deposit value too low");
       });
 
       it("reverts when stakeAmount is not multiple of gwei", async () => {
-        const stakeAmount = ethers.BigNumber.from(String(1e18)).add(ethers.BigNumber.from(String(1)));
-        await expect(testContract.getDepositDataRoot(pubkey, withdrawal_credentials, signature, stakeAmount)).to.be.revertedWith(
+        const stakeAmount = ethers.BigNumber.from(String(1e18)).add(
+          ethers.BigNumber.from(String(1))
+        );
+        await expect(
+          testContract.getDepositDataRoot(
+            pubkey,
+            withdrawalCredentials,
+            signature,
+            stakeAmount
+          )
+        ).to.be.revertedWith(
           "DepositContract: deposit value not multiple of gwei"
         );
       });
 
       it("calculates depositDataRoot correctly with stakeAmount 32 ether", async () => {
         const stakeAmount = ethers.BigNumber.from(String(32e18));
-        const response = await testContract.getDepositDataRoot(pubkey, withdrawal_credentials, signature, stakeAmount);
+        const response = await testContract.getDepositDataRoot(
+          pubkey,
+          withdrawalCredentials,
+          signature,
+          stakeAmount
+        );
         expect(response).to.eq(depositDataRoot);
       });
     });
@@ -66,8 +89,6 @@ describe("DepositContractUtils", async () => {
     it("to show it is possible to get any const internal variable with example of PUBKEY_LENGTH", async () => {
       const response = await testContract.getPubkeyLength();
       expect(response).to.eq(48);
-    })
-
+    });
   });
 });
-
