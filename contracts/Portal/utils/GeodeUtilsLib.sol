4dfdb9c6e05a88a321aa5789b0f3a0eb7ff56174
geode-main <admin@geode.fi>
2022-12-22 05:58:48 +0300

    Feat: GeodeUtils - MAX_GOVERNANCE_FEE is now constant

--- contracts/Portal/utils/GeodeUtilsLib.sol
+++ contracts/Portal/utils/GeodeUtilsLib.sol
@@ -17,8 +17,7 @@ import "./DataStoreUtilsLib.sol";
 library GeodeUtils {
     using DataStoreUtils for DataStoreUtils.DataStore;
 
-    event GovernanceTaxUpdated(uint256 newFee);
-    event MaxGovernanceTaxUpdated(uint256 newMaxFee);
+    event GovernanceFeeUpdated(uint256 newFee);
     event ControllerChanged(uint256 id, address newCONTROLLER);
     event Proposed(
         uint256 id,
@@ -65,8 +64,7 @@ library GeodeUtils {
      * Suggests updates, such as new planets, operators, comets, contract upgrades and new Senate, on the Ecosystem -without any permission to force them-
      * @param SENATE An address that controls the state of governance, updates and other users in the Geode Ecosystem
      * Note SENATE is proposed by Governance and voted by all elector types, operates if ⌊2/3⌋ approves.
-     * @param GOVERNANCE_TAX operation fee of the given contract, acquired by GOVERNANCE. Limited by MAX_GOVERNANCE_TAX
-     * @param MAX_GOVERNANCE_TAX set by SENATE, limited by PERCENTAGE_DENOMINATOR
+     * @param GOVERNANCE_FEE operation fee of the given contract, acquired by GOVERNANCE. Limited by MAX_GOVERNANCE_FEE
      * @param SENATE_EXPIRY refers to the last timestamp that SENATE can continue operating. Enforces a new election, limited by MAX_SENATE_PERIOD
      * @param approvedUpgrade only 1 implementation contract can be "approved" at any given time. @dev safe to set to address(0) after every upgrade
      * @param _electorCount increased when a new id is added with _electorTypes[id] == true
@@ -77,8 +75,7 @@ library GeodeUtils {
     struct Universe {
         address SENATE;
         address GOVERNANCE;
-        uint256 GOVERNANCE_TAX;
-        uint256 MAX_GOVERNANCE_TAX;
+        uint256 GOVERNANCE_FEE;
         uint256 SENATE_EXPIRY;
         address approvedUpgrade;
         uint256 _electorCount;
@@ -89,6 +86,9 @@ library GeodeUtils {
 
     /// @notice PERCENTAGE_DENOMINATOR represents 100%
     uint256 public constant PERCENTAGE_DENOMINATOR = 10**10;
+
+    uint256 public constant MAX_GOVERNANCE_FEE =
+        (PERCENTAGE_DENOMINATOR * 5) / 100;
 
     uint32 public constant MIN_PROPOSAL_DURATION = 1 days;
     uint32 public constant MAX_PROPOSAL_DURATION = 2 weeks;
@@ -145,26 +145,15 @@ library GeodeUtils {
     }
 
     /**
-     * @notice MAX_GOVERNANCE_TAX must limit GOVERNANCE_TAX even if MAX is changed
-     * @return active GOVERNANCE_TAX, limited by MAX_GOVERNANCE_TAX
+     * @notice MAX_GOVERNANCE_FEE must limit GOVERNANCE_FEE even if MAX is changed
+     * @return active GOVERNANCE_FEE, limited by MAX_GOVERNANCE_FEE
      */
-    function getGovernanceTax(Universe storage self)
+    function getGovernanceFee(Universe storage self)
         external
         view
         returns (uint256)
     {
-        return self.GOVERNANCE_TAX;
-    }
-
-    /**
-     *  @return MAX_GOVERNANCE_TAX
-     */
-    function getMaxGovernanceTax(Universe storage self)
-        external
-        view
-        returns (uint256)
-    {
-        return self.MAX_GOVERNANCE_TAX;
+        return self.GOVERNANCE_FEE;
     }
 
     /**
@@ -183,44 +172,23 @@ library GeodeUtils {
      */
 
     /**
-     * @dev can not set the fee more than MAX_GOVERNANCE_TAX
-     * @dev no need to check PERCENTAGE_DENOMINATOR because MAX_GOVERNANCE_TAX is limited already
+     * @dev can not set the fee more than MAX_GOVERNANCE_FEE
+     * @dev no need to check PERCENTAGE_DENOMINATOR because MAX_GOVERNANCE_FEE is limited already
      * @return true if the operation was succesful, might be helpful when governance rights are distributed
      */
-    function setGovernanceTax(Universe storage self, uint256 newFee)
+    function setGovernanceFee(Universe storage self, uint256 newFee)
         external
         onlyGovernance(self)
         returns (bool)
     {
         require(
-            newFee <= self.MAX_GOVERNANCE_TAX,
-            "GeodeUtils: cannot be more than MAX_GOVERNANCE_TAX"
+            newFee <= MAX_GOVERNANCE_FEE,
+            "GeodeUtils: cannot be more than MAX_GOVERNANCE_FEE"
         );
 
-        self.GOVERNANCE_TAX = newFee;
+        self.GOVERNANCE_FEE = newFee;
 
-        emit GovernanceTaxUpdated(newFee);
-
-        return true;
-    }
-
-    /**
-     * @dev can not set a fee more than PERCENTAGE_DENOMINATOR (100%)
-     * @return true if the operation was succesful
-     */
-    function setMaxGovernanceTax(Universe storage self, uint256 newMaxFee)
-        external
-        onlySenate(self)
-        returns (bool)
-    {
-        require(
-            newMaxFee <= PERCENTAGE_DENOMINATOR,
-            "GeodeUtils: fee more than 100%"
-        );
-
-        self.MAX_GOVERNANCE_TAX = newMaxFee;
-
-        emit MaxGovernanceTaxUpdated(newMaxFee);
+        emit GovernanceFeeUpdated(newFee);
 
         return true;
     }
