4a4127d1fc347d7da28691aec7717d04b454f33e
geode-main <admin@geode.fi>
2023-04-09 07:28:46 +0300

    Refactor: a better globals folder

--- contracts/Portal/globals/id_type.sol
+++ contracts/Portal/globals/id_type.sol
@@ -2,10 +2,37 @@
 pragma solidity =0.8.7;
 
 /**
- * @notice ID_TYPE is like an ENUM, widely used within Portal and Modules like Withdrawal Contract
+ * @notice ID_TYPE is an internal library that acts like an ENUM.
+ *
  * @dev Why not use enums, they basically do the same thing?
  * * We like using explicitly defined uints than linearly increasing ones.
- * @dev all LIMIT parameters are exclusive, this aims to prevent double usage.
+ *
+ * @dev Used within the limited upgradability pattern:
+ * * Proposal & User TYPEs:
+ * * * NONE & GAP: should not be used tbh.
+ * * * SENATE: points to a proposal that will update the current SENATE address of a package or Portal.
+ * * * CONTRACT_UPGRADE: proposal to change the given contract's implementation.
+ * * * OPERATOR: permissionned Node Operators (hosted on Portal).
+ * * * POOL: permissionless staking pools (hosted on Portal).
+ *
+ * * Packages: (hosted on StakeModule)
+ * * * An ID can only point to 1(one) Package implementation at a time.
+ * * * Can be upgraded by a dual governance, via pullUpgrade.
+ * * * * Portal's dual governance consists of a Governance Token(governance) and a Senate(senate).
+ * * * * A Package's dual governance consists of Portal(governance) and the pool owner(senate).
+ * * * Built by utilizing the Modules.
+ * * * LiquidityPool and WithdrawalPool are some examples.
+ *
+ * * Middlewares: (hosted on StakeModule)
+ * * * An ID can point to multiple Middleware implementations at the same time.
+ * * * Can not be upgraded.
+ * * * Do not have any guides to build really.
+ * * * Currently only gETHMiddlewares
+ *
+ * * Limits:
+ * * * We simply set limits to separate a Package from a Middleware
+ *
+ * @dev all LIMIT parameters are exclusive, prevents double usage.
  */
 library ID_TYPE {
   /// @notice TYPE 0: *invalid*
@@ -17,7 +44,7 @@ library ID_TYPE {
   /// @notice TYPE 2: Contract Upgrade
   uint256 internal constant CONTRACT_UPGRADE = 2;
 
-  /// @notice TYPE 3: *gap*: formally represented the admin contract, now reserved to be never used
+  /// @notice TYPE 3: *gap*: formally represented the admin contract. reserved to be never used.
   uint256 internal constant __GAP__ = 3;
 
   /// @notice TYPE 4: USER: Node Operators
@@ -28,29 +55,26 @@ library ID_TYPE {
 
   /// --
 
-  /// @notice TYPE 10000: LIMIT: exclusive, minimum TYPE that will be percieved as a default module
-  uint256 internal constant LIMIT_DEFAULT_MODULE_MIN = 10000;
+  /// @notice TYPE 10000: Limit: exclusive, minimum TYPE that will be percieved as a package
+  uint256 internal constant LIMIT_MIN_PACKAGE = 10000;
 
-  /// @notice TYPE 10011: Module: The Withdrawal Contract
-  uint256 internal constant DEFAULT_MODULE_WITHDRAWAL_CONTRACT = 10011;
+  /// @notice TYPE 10011: Package: The Withdrawal Credential Contract
+  uint256 internal constant PACKAGE_WITHDRAWAL_POOL = 10011;
 
-  /// @notice TYPE 10021: Module: A Liquidity Pool
-  uint256 internal constant DEFAULT_MODULE_LIQUDITY_POOL = 10021;
+  /// @notice TYPE 10021: Package: A Liquidity Pool
+  uint256 internal constant PACKAGE_LIQUDITY_POOL = 10021;
 
-  // /// @notice TYPE 10021: Module: A Liquidity Pool Token
-  // uint256 internal constant DEFAULT_MODULE_LIQUDITY_POOL_TOKEN = 10022;
-
-  /// @notice TYPE 19999: LIMIT: exclusive, maximum TYPE that will be percieved as a default module
-  uint256 internal constant LIMIT_DEFAULT_MODULE_MAX = 19999;
+  /// @notice TYPE 19999: Limit: exclusive, maximum TYPE that will be percieved as a package
+  uint256 internal constant LIMIT_MAX_PACKAGE = 19999;
 
   /// --
 
-  /// @notice TYPE 20000: LIMIT: exclusive, minimum TYPE that will be percieved as a allowed module
-  uint256 internal constant LIMIT_ALLOWED_MODULE_MIN = 20000;
+  /// @notice TYPE 20000: Limit: exclusive, minimum TYPE that will be percieved as a middleware
+  uint256 internal constant LIMIT_MIN_MIDDLEWARE = 20000;
 
-  /// @notice TYPE 20031: Module: A new gETH interface
-  uint256 internal constant ALLOWED_MODULE_GETH_INTERFACE = 20031;
+  /// @notice TYPE 20031: Middleware: A new gETH interface
+  uint256 internal constant MIDDLEWARE_GETHMIDDLEWARE = 20011;
 
-  /// @notice TYPE 29999: LIMIT: exclusive, maximum TYPE that will be percieved as a allowed module
-  uint256 internal constant LIMIT_ALLOWED_MODULE_MAX = 29999;
+  /// @notice TYPE 29999: Limit: exclusive, maximum TYPE that will be percieved as a middleware
+  uint256 internal constant LIMIT_MAX_MIDDLEWARE = 29999;
 }
--- contracts/Portal/globals/validator_state.sol
+++ contracts/Portal/globals/validator_state.sol
@@ -2,21 +2,24 @@
 pragma solidity =0.8.7;
 
 /**
- * @notice VALIDATOR_STATE keeping track of validators within The Staking Library
+ * @notice VALIDATOR_STATE: keeping track of validators within The Staking Library.
  */
 library VALIDATOR_STATE {
   /// @notice STATE 0: *invalid*
   uint8 internal constant NONE = 0;
 
-  /// @notice STATE 1: validator is proposed, 1 ETH is sent from Operator to Deposit Contract
+  /// @notice STATE 1: validator is proposed, 1 ETH is sent from Operator to Deposit Contract.
   uint8 internal constant PROPOSED = 1;
 
-  /// @notice STATE 2: proposal was approved, operator used pooled funds, 1 ETH is released back to Operator
+  /// @notice STATE 2: proposal was approved, operator used pooled funds, 1 ETH is released back to Operator.
   uint8 internal constant ACTIVE = 2;
 
-  /// @notice STATE 3: validator is exited, not currently used much
-  uint8 internal constant EXITED = 3;
+  /// @notice STATE 3: validator is called to be exited. Currently not used.
+  uint8 internal constant EXIT_CALLED = 3;
 
-  /// @notice STATE 69: proposal was malicious(alien), maybe faulty signatures or probably: (https://bit.ly/3Tkc6UC)
+  /// @notice STATE 3: validator is fully exited. Currently not used.
+  uint8 internal constant EXIT_FILLED = 4;
+
+  /// @notice STATE 69: proposal was malicious(alien). Maybe faulty signatures or probably (https://bit.ly/3Tkc6UC)
   uint8 internal constant ALIENATED = 69;
 }
