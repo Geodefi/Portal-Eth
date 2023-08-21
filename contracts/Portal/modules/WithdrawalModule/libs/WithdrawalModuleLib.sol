7f086358f6ff471a890e23b84b9de635f96301ef
geode-main <admin@geode.fi>
2023-08-21 15:37:53 +0300

    Feat: initiate Withdrawal Module

--- contracts/Portal/modules/WithdrawalModule/libs/WithdrawalModuleLib.sol
+++ contracts/Portal/modules/WithdrawalModule/libs/WithdrawalModuleLib.sol
@@ -1,7 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.7;
 
+// globals
+// libraries
 import {DataStoreModuleLib as DSML} from "../../DataStoreModule/libs/DataStoreModuleLib.sol";
+// interfaces
+import {IgETH} from "../../../interfaces/IgETH.sol";
+import {IPortal} from "../../../interfaces/IPortal.sol";
+// external
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 library WithdrawalModuleLib {
   using DSML for DSML.IsolatedStorage;
@@ -10,4 +17,3 @@ library WithdrawalModuleLib {
     uint256 pooledTokenId;
     uint256[15] __gap;
   }
-}
