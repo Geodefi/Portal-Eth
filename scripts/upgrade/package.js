2be9b2369115c008a2ef3266c2ffc576121be9d4
crashbandicoott <bandicoot@geode.fi>
2023-10-24 17:30:19 +0200

    Test: add liquidity pool package upgradability test

--- scripts/upgrade/package.js
+++ scripts/upgrade/package.js
@@ -2,6 +2,7 @@
 const { delay } = require("../../utils");
 
 const TYPE_PACKAGE_WITHDRAWAL_CONTRACT = 10011;
+const TYPE_LIQUIDITY_POOL_CONTRACT = 10021;
 const MAX_PROPOSAL_DURATION = 2419200;
 const DELAY_SECONDS = 100;
 
@@ -38,7 +39,7 @@ module.exports.upgradePackage = async function (hre, portal, version = "V1_0") {
     const newImplementationAddress = await upgrades.prepareUpgrade(currentWC, WCFactory, {
       redeployImplementation: "onchange",
       kind: "uups",
-      unsafeAllow: ["external-library-linking", "state-variable-immutable"], // TODO: check/remove state-variable-immutable
+      unsafeAllow: ["external-library-linking", "state-variable-immutable"],
       constructorArgs: [(await get("gETH")).address, portal.address],
     });
 
@@ -57,16 +58,73 @@ module.exports.upgradePackage = async function (hre, portal, version = "V1_0") {
 
     await portal.approveProposal(_id);
     await delay(DELAY_SECONDS);
+  } catch (error) {
+    console.log(error);
+    console.log("Unsuccesful upgrade...");
+    console.log("try --network");
+  }
+};
 
-    // await poolWC.connect(signer).pullUpgrade();
-    // await delay(DELAY_SECONDS);
+module.exports.upgradeLPPackage = async function (hre, portal, version = "V1_0") {
+  try {
+    const { ethers, upgrades, deployments } = hre;
+    const { get } = deployments;
 
-    // const upgradedWithdrawalContract = await ethers.getContractAt(
-    //   "WithdrawalContract" + version,
-    //   currentWC.target
-    // );
+    const oldLPFactory = await ethers.getContractFactory("LiquidityPool", {
+      libraries: {
+        GeodeModuleLib: (await get("GeodeModuleLib")).address,
+        LiquidityModuleLib: (await get("LiquidityModuleLib")).address,
+      },
+    });
 
-    // return upgradedWithdrawalContract;
+    const LPFactory = await ethers.getContractFactory("LiquidityPool" + version, {
+      libraries: {
+        GeodeModuleLib: (await get("GeodeModuleLib")).address,
+        LiquidityModuleLib: (await get("LiquidityModuleLib")).address,
+      },
+    });
+
+    const currentLP = await upgrades.forceImport(
+      (
+        await get("LiquidityPool")
+      ).address,
+      oldLPFactory,
+      {
+        kind: "uups",
+        constructorArgs: [
+          (await get("gETH")).address,
+          portal.address,
+          (await get("LPToken")).address,
+        ],
+      }
+    );
+
+    const newImplementationAddress = await upgrades.prepareUpgrade(currentLP, LPFactory, {
+      redeployImplementation: "onchange",
+      kind: "uups",
+      unsafeAllow: ["external-library-linking", "state-variable-immutable"],
+      constructorArgs: [
+        (await get("gETH")).address,
+        portal.address,
+        (await get("LPToken")).address,
+      ],
+    });
+
+    await delay(DELAY_SECONDS);
+
+    const _name = web3.utils.asciiToHex(version);
+    const _id = await portal.generateId(version, TYPE_LIQUIDITY_POOL_CONTRACT);
+
+    await portal.propose(
+      newImplementationAddress,
+      TYPE_LIQUIDITY_POOL_CONTRACT,
+      _name,
+      MAX_PROPOSAL_DURATION
+    );
+    await delay(DELAY_SECONDS);
+
+    await portal.approveProposal(_id);
+    await delay(DELAY_SECONDS);
   } catch (error) {
     console.log(error);
     console.log("Unsuccesful upgrade...");
--- test/upgradability/package.test.js
+++ test/upgradability/package.test.js
@@ -2,15 +2,15 @@ const { expect } = require("chai");
 
 const { BN } = require("@openzeppelin/test-helpers");
 const { strToBytes, strToBytes32, generateId, DAY } = require("../../utils");
-const { upgradePackage } = require("../../scripts/upgrade/package");
+const { upgradePackage, upgradeLPPackage } = require("../../scripts/upgrade/package");
 
 const ERC1967Proxy = artifacts.require("ERC1967Proxy");
 
 const Portal = artifacts.require("Portal");
 const gETH = artifacts.require("gETH");
-// const LPToken = artifacts.require("LPToken");
+const LPToken = artifacts.require("LPToken");
 
-// const LiquidityModuleLib = artifacts.require("LiquidityModuleLib");
+const LiquidityModuleLib = artifacts.require("LiquidityModuleLib");
 const GeodeModuleLib = artifacts.require("GeodeModuleLib");
 const OracleExtensionLib = artifacts.require("OracleExtensionLib");
 const StakeModuleLib = artifacts.require("StakeModuleLib");
@@ -18,33 +18,37 @@ const InitiatorExtensionLib = artifacts.require("InitiatorExtensionLib");
 
 const WithdrawalModuleLib = artifacts.require("WithdrawalModuleLib");
 const WithdrawalContract = artifacts.require("WithdrawalContract");
-// const LiquidityPool = artifacts.require("LiquidityPool");
+const LiquidityPool = artifacts.require("LiquidityPool");
 
 contract("UpgradePackages", function (accounts) {
   const [deployer, poolOwner] = accounts;
   let upgradeWithdrawalPackage;
-  let poolId;
   let upgradedPoolWC;
+  let upgradedPoolLP;
   let portal;
+  let gETHContract;
+  let tokenId;
 
   let poolOwnerSigner;
 
   const setupTest = deployments.createFixture(async (hre) => {
     poolOwnerSigner = await ethers.getSigner(poolOwner);
 
-    this.gETH = await gETH.new("name", "symbol", "uri", { from: deployer });
+    gETHContract = await gETH.new("name", "symbol", "uri", { from: deployer });
     const portalImp = await Portal.new();
     const { address } = await ERC1967Proxy.new(portalImp.address, "0x");
     portal = await Portal.at(address);
-    await portal.initialize(deployer, deployer, this.gETH.address, deployer, strToBytes("v1"));
+    await portal.initialize(deployer, deployer, gETHContract.address, deployer, strToBytes("v1"));
 
     // set up the portal to gETH
-    await this.gETH.transferMinterRole(portal.address);
-    await this.gETH.transferMiddlewareManagerRole(portal.address);
-    await this.gETH.transferOracleRole(portal.address);
+    await gETHContract.transferMinterRole(portal.address);
+    await gETHContract.transferMiddlewareManagerRole(portal.address);
+    await gETHContract.transferOracleRole(portal.address);
+  });
 
+  const setupWCTest = deployments.createFixture(async (hre) => {
     // set WC as middleware
-    const wc = await WithdrawalContract.new(this.gETH.address, portal.address);
+    const wc = await WithdrawalContract.new(gETHContract.address, portal.address);
     await portal.propose(wc.address, 10011, strToBytes("name"), DAY, { from: deployer });
     await portal.approveProposal(await generateId(strToBytes("name"), 10011), {
       from: deployer,
@@ -61,8 +65,7 @@ contract("UpgradePackages", function (accounts) {
     await upgradePackage(hre, portal, "V2_0_Mock");
 
     // pull the upgrade
-    poolId = await generateId(strToBytes("name"), 5);
-    const WCAddress = await portal.readAddress(poolId, strToBytes32("withdrawalContract"));
+    const WCAddress = await portal.readAddress(tokenId, strToBytes32("withdrawalContract"));
     const poolWC = await ethers.getContractAt("WithdrawalContract", WCAddress);
     await poolWC.connect(poolOwnerSigner).pullUpgrade();
 
@@ -71,13 +74,54 @@ contract("UpgradePackages", function (accounts) {
     await upgradedPoolWC.connect(poolOwnerSigner).initializeV2_0_Mock(8);
   });
 
+  const setupLPTest = deployments.createFixture(async (hre) => {
+    // set WC as middleware
+    const wc = await WithdrawalContract.new(gETHContract.address, portal.address);
+    await portal.propose(wc.address, 10011, strToBytes("name"), DAY, { from: deployer });
+    await portal.approveProposal(await generateId(strToBytes("name"), 10011), {
+      from: deployer,
+    });
+
+    // set LP as middleware
+    const lpImp = await LPToken.new();
+    const lp = await LiquidityPool.new(gETHContract.address, portal.address, lpImp.address);
+    await portal.propose(lp.address, 10021, strToBytes("name"), DAY, {
+      from: deployer,
+    });
+    await portal.approveProposal(await generateId(strToBytes("name"), 10021), {
+      from: deployer,
+    });
+
+    // initiate pool
+    await portal.initiatePool(0, 0, poolOwner, strToBytes("name"), "0x", [false, false, false], {
+      from: poolOwner,
+      value: new BN(String(1e18)).muln(32),
+    });
+
+    // deploy LP for pool
+    await portal.deployLiquidityPool(tokenId, { from: poolOwner });
+
+    // upgrade the LP with proposal on portal
+    await deployments.fixture(["LiquidityPool"]);
+    await upgradeLPPackage(hre, portal, "V2_0_Mock");
+
+    // pull the upgrade
+    const LPAddress = await portal.readAddress(tokenId, strToBytes32("liquidityPool"));
+    const poolLP = await ethers.getContractAt("LiquidityPool", LPAddress);
+    await poolLP.connect(poolOwnerSigner).pullUpgrade();
+
+    // set upgraded WC and initialize
+    upgradedPoolLP = await ethers.getContractAt("LiquidityPoolV2_0_Mock", LPAddress);
+    await upgradedPoolLP.connect(poolOwnerSigner).initializeV2_0_Mock(8);
+  });
+
   before(async function () {
     this.SML = await StakeModuleLib.new();
     // this should be before --> await InitiatorExtensionLib.new();
     await InitiatorExtensionLib.link(this.SML);
     this.IEL = await InitiatorExtensionLib.new();
     this.OEL = await OracleExtensionLib.new();
-    // this.LML = await LiquidityModuleLib.new();
+    this.LML = await LiquidityModuleLib.new();
     this.GML = await GeodeModuleLib.new();
     this.WML = await WithdrawalModuleLib.new();
 
@@ -86,13 +130,13 @@ contract("UpgradePackages", function (accounts) {
     await Portal.link(this.OEL);
     await Portal.link(this.IEL);
 
-    // await LiquidityPool.link(this.GML);
-    // await LiquidityPool.link(this.LML);
+    await LiquidityPool.link(this.GML);
+    await LiquidityPool.link(this.LML);
 
     await WithdrawalContract.link(this.GML);
     await WithdrawalContract.link(this.WML);
 
-    // tokenId = await generateId(strToBytes("name"), 5);
+    tokenId = await generateId(strToBytes("name"), 5);
   });
 
   beforeEach(async function () {
@@ -100,6 +144,9 @@ contract("UpgradePackages", function (accounts) {
   });
 
   describe("Upgrade WC Package", async function () {
+    beforeEach(async function () {
+      await setupWCTest();
+    });
     it("can use reinitializer: freshSlot = 8", async function () {
       expect((await upgradedPoolWC.getFreshSlot()).toString()).to.be.equal(String(8));
     });
@@ -114,6 +161,27 @@ contract("UpgradePackages", function (accounts) {
     it("can add new function with parameter: setFreshSlot, freshSlot", async function () {
       await upgradedPoolWC.setFreshSlot(3);
       expect((await upgradedPoolWC.getFreshSlot()).toString()).to.be.equal(String(3));
+    });
+  });
+
+  describe("Upgrade LP Package", async function () {
+    beforeEach(async function () {
+      await setupLPTest();
+    });
+    it("can use reinitializer: freshSlot = 8", async function () {
+      expect((await upgradedPoolLP.getFreshSlot()).toString()).to.be.equal(String(8));
+    });
+
+    it("Upgrade works correctly: LP version = V2_0_Mock", async function () {
+      const version = await upgradedPoolLP.getContractVersion();
+      expect(version.toString()).to.equal(
+        (await generateId(strToBytes("V2_0_Mock"), 10021)).toString()
+      );
+    });
+
+    it("can add new function with parameter: setFreshSlot, freshSlot", async function () {
+      await upgradedPoolLP.setFreshSlot(3);
+      expect((await upgradedPoolLP.getFreshSlot()).toString()).to.be.equal(String(3));
     });
   });
 });
